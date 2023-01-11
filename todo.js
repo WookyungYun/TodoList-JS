e5fbb2c6f3d40289e3c6bd4231d25e1577c5dea5
WookyungYun <gyong1540@gmail.com>
2023-01-11 22:48:57 +0900

    feat: 삭제 구현

--- todo.js
+++ todo.js
@@ -3,12 +3,23 @@ const todoInput = document.querySelector(".todoInput");
 const todos = document.querySelector(".todos");
 
 let todoList = [];
+function deleteTodo(e) {
+  const removeTodo = e.target.parentNode;
+  todos.removeChild(removeTodo);
+  todoList = todoList.filter((todo) => todo.id !== +removeTodo.id);
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+}
 
 function addTodo(todo) {
   const list = document.createElement("li");
   const todoItem = document.createElement("span");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = "삭제";
+  deleteBtn.addEventListener("click", deleteTodo);
   todoItem.innerHTML = todo;
   list.appendChild(todoItem);
+  list.appendChild(deleteBtn);
+  list.id = todoList.length + 1;
   todos.appendChild(list);
 }
 
@@ -18,8 +29,8 @@ function createTodo(e) {
   addTodo(todo);
   let date = new Date();
   const todoObj = {
-    text: todo,
     id: todoList.length + 1,
+    text: todo,
     date: date,
   };
   todoList.push(todoObj);
