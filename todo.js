a4683b78ea6d177743cdf718cd9f32981427a6c0
WookyungYun <gyong1540@gmail.com>
2023-01-11 21:43:37 +0900

    feat: 할 일 추가

--- todo.js
+++ todo.js
@@ -1,3 +1,31 @@
 const todoForm = document.querySelector(".todoForm");
 const todoInput = document.querySelector(".todoInput");
 const todos = document.querySelector(".todos");
+
+let todoList = [];
+
+function addTodo(todo) {
+  const list = document.createElement("li");
+  const todoItem = document.createElement("span");
+  todoItem.innerHTML = todo;
+  list.appendChild(todoItem);
+  todos.appendChild(list);
+}
+
+function createTodo(e) {
+  e.preventDefault();
+  const todo = todoInput.value;
+  addTodo(todo);
+  const todoObj = {
+    text: todo,
+    id: todoList.length + 1,
+  };
+  todoList.push(todoObj);
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+  todoInput.value = "";
+}
+
+function init() {
+  todoForm.addEventListener("submit", createTodo);
+}
+init();
