a7be907c5e3f3bad9d61dfc6034e2139567815ee
WookyungYun <gyong1540@gmail.com>
2023-01-12 00:23:57 +0900

    feat:수정 기능 구현

--- todo.js
+++ todo.js
@@ -3,6 +3,27 @@ const todoInput = document.querySelector(".todoInput");
 const todos = document.querySelector(".todos");
 
 let todoList = [];
+
+function editTodo(e, todoItem) {
+  const todoItemId = todoItem.parentNode.id;
+  const editValue = todoItem.value;
+  let editDate = new Date();
+  if (e.target.innerHTML === "수정") {
+    e.target.innerHTML = "저장";
+    todoItem.removeAttribute("disabled");
+    todoItem.focus();
+  } else {
+    todoList = todoList.map((todo) =>
+      todo.id === +todoItemId
+        ? { ...todo, text: editValue, date: editDate }
+        : todo
+    );
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+    todoItem.setAttribute("disabled", true);
+    e.target.innerHTML = "수정";
+  }
+}
+
 function deleteTodo(e) {
   const removeTodo = e.target.parentNode;
   todos.removeChild(removeTodo);
@@ -12,12 +33,17 @@ function deleteTodo(e) {
 
 function addTodo(todo) {
   const list = document.createElement("li");
-  const todoItem = document.createElement("span");
+  const todoItem = document.createElement("input");
+  todoItem.value = todo;
+  todoItem.setAttribute("disabled", true);
   const deleteBtn = document.createElement("button");
   deleteBtn.innerHTML = "삭제";
   deleteBtn.addEventListener("click", deleteTodo);
-  todoItem.innerHTML = todo;
+  const editBtn = document.createElement("button");
+  editBtn.innerHTML = "수정";
+  editBtn.addEventListener("click", (e) => editTodo(e, todoItem));
   list.appendChild(todoItem);
+  list.appendChild(editBtn);
   list.appendChild(deleteBtn);
   list.id = todoList.length + 1;
   todos.appendChild(list);
